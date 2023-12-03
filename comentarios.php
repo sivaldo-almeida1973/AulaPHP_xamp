144d69bea651ef74a98a7f8f5827a0537a8df2d4
Sivaldo Vieira <vieiralmeida.com@hotmail.com>
2023-12-03 17:11:57 -0300

    criado pagina comentarios

---
+++ comentarios.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+  <body>
+    <!-- comentarios HTML-->
+
+      <?php
+         //Comentario de 1 linha
+         #Comentario de 1 linha
+
+          echo 'Comando echo ............ <br/>';
+          print ('Comando print <hr/>');
+          echo print ('Comando print <hr/>');
+
+
+          /*
+          comentario de multiplas linhas.
+          Este tipo de comentario permite que varias
+          linhas sejam comentadas ao mesmo tempo
+          */
+
+        ?>
+      
+
+    
+    
+  </body>
+</html>
