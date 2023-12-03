2bb83aa4a91126c860a5dd88f910b10ec4ee1d34
Sivaldo Vieira <vieiralmeida.com@hotmail.com>
2023-12-03 15:08:25 -0300

    criado paginas inde.php e echo_print.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+  <body>
+    <?php
+       echo"utilizando tag padrão!";
+
+    ?>
+    <br>
+    <?='Utilizando a tag de impressão!'?>
+    <br>
+    <? echo 'Utilizando a tag curta' ?>
+
+
+    
+  </body>
+</html>
