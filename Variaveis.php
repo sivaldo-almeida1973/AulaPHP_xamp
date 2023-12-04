66fca47b8d415414cdbc8a46fcaf056fe41e2d59
Sivaldo Vieira <vieiralmeida.com@hotmail.com>
2023-12-03 21:26:21 -0300

    criado pagina variaveis.php

---
+++ Variaveis.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+  <body>
+     <?php
+        //String
+        $nome = 'Sivaldo vieira';
+
+        //Int
+        $idade = 23;
+
+        //float
+        $peso = 85.5;
+
+        //boolean
+        $fumante_sn = true;//(true = 1, false = 0)
+     
+     ?>
+
+     <h1>Ficha cadastral</h1>
+     <br>
+     <h5>Nome: <?= $nome ?> </h5>
+     <h5>Idade: <?= $idade ?> </h5>
+     <h5>Peso: <?= $peso ?> </h5>
+     <h5>Fumante: <?= $fumante_sn ?> </h5>
+    
+ 
+    
+  </body>
+</html>
