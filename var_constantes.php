c9f97501d09745225cea4b22facda19603a4e81b
Sivaldo Vieira <vieiralmeida.com@hotmail.com>
2023-12-04 10:12:27 -0300

    criado pagina var_contantes.php

---
+++ var_constantes.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Curso PHP</title>
+</head>
+  <body>
+      <?php
+         define("BD_URL","endereco_bd_dev");
+         define("BD_USUARIO","usuario_dev");
+         define("BD_SENHA","senha_dev");
+
+         // ..logica..//
+         
+
+          ECHO BD_URL.'<br/>';
+          ECHO BD_USUARIO.'<br/>';
+          ECHO BD_SENHA.'<br/>';
+
+      
+      ?>
+  </body>
+</html>
