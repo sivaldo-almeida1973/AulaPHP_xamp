6621c9c0177ef4ce30f0f37257f2f45867fdd0eb
Sivaldo Vieira <vieiralmeida.com@hotmail.com>
2023-12-04 20:54:07 -0300

    Criado pagina Casting_de_tipos.php

---
+++ Casting_de_tipos.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Curso PHP</title>
+</head>
+  <body>
+    <?php
+    //Fazendo Casting------------------------------------
+      //gettype() => retorna o tipo da variável
+      //$valor = 10.55;
+      //$valor2 = (double) $valor;
+      //$valor2 = (string) $valor;
+      //$valor2 = (int) $valor;
+
+
+      //$valor = '22.12';
+      //$valor = 'abc';
+      //$valor = ''; retorna false
+
+      //$valor2 = (integer) $valor;
+      //$valor2 = (string) $valor;
+      //$valor2 = (bool) $valor;
+
+
+      $valor = true;
+      //$valor = false;
+
+      $valor2 = (string) $valor;
+      $valor3 = (int) $valor2;
+
+      //imprime na tela
+      echo $valor.' '.gettype($valor);
+      echo '<br/>';
+      echo $valor2.' '.gettype($valor2);
+      echo '<br/>';
+      echo $valor3.' '.gettype($valor3);
+
+
+    ?>
+    
+     
+
+  </body>
+</html>
