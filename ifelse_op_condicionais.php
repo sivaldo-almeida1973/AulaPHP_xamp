894eec772c91b1f1630f898d97f6694ad1237a02
Sivaldo Vieira <vieiralmeida.com@hotmail.com>
2023-12-04 10:56:07 -0300

    criado pagina ifelse_op_condicionais.php

---
+++ ifelse_op_condicionais.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Curso PHP</title>
+</head>
+  <body>
+      <?php
+        // == igual, === identico , != ou <> diferente
+        // !== não identico
+        // < menor, 
+        // <= menor ou igual
+        // > maior,  
+        // >= maior ou igual
+        
+
+         if(4>3){
+             echo "Verdadeiro";
+
+         } else {
+              echo "Falso";
+         }
+
+      
+      ?>
+  </body>
+</html>
