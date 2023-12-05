c2875f908196f9806d3e9f711e4848cd66154937
Sivaldo Vieira <vieiralmeida.com@hotmail.com>
2023-12-05 09:29:17 -0300

    Oper_atribuicao_aritimetico.ph criado pagina

---
+++ Oper_atribuicao_aritimeticos.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Curso PHP</title>
+</head>
+  <body>
+    <?php
+       //=,+,-,/,%
+
+       $x = 10;
+       $y = 8;
+
+
+       //$x += $y;
+       //$x -= $y;
+       // $x *= $y;
+       //$x /= $y;
+       $x %= $y;
+
+
+
+
+       echo $x;
+    
+
+   
+    ?>
+    
+     
+
+  </body>
+</html>
