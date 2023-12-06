e54cea35bac179cd4c27b0b09b412f3d926f8b26
Sivaldo Vieira <vieiralmeida.com@hotmail.com>
2023-12-06 12:52:15 -0300

    criado pagina funcoes_nativas_matematicas.php

---
+++ funcoes_nativas_matematicas.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Curso PHP</title>
+</head>
+  <body>
+    <h3>Funções</h3>
+    <?php
+    /*
+      //ceil arredonda pra cima  ------------------------
+        $num = 7.499;
+        $num2 = 9.9;
+        $num3 = -7.3;  //-7
+        echo ceil($num);
+        echo '<hr>';
+        echo ceil($num2);
+        echo '<hr>';
+        echo ceil($num3);
+
+         //floor arredonda pra baixo  ------------------------
+         echo '<hr>';
+         echo floor($num);
+         echo '<hr>';
+         echo floor($num2);
+         echo '<hr>';
+         echo floor($num3); //-8
+         
+         //round arredonda pra baixo se estiver na metade pra baixo, e pracima se estiver da metade pra cima ------------------------
+
+         //ex 7,499 = 7
+         //ex7.5 = 8
+         echo '<hr>';
+         echo round($num);
+         echo '<hr>';
+         echo round($num2);
+         echo '<hr>';
+         echo round($num3); //-7
+    
+         //rand() numero aleatorio
+         echo '<hr>';
+         echo rand(10, 20);//com parametros
+         echo '<br>'.getrandmax();
+       */
+
+       //sqrt() raiz quadrada
+         echo sqrt(10);
+         echo '<hr>';
+         echo sqrt(100);
+         echo '<hr>';
+         echo sqrt(1000);
+         echo '<hr>';
+         echo sqrt(10000);
+         echo '<hr>';
+         echo sqrt(100000);
+
+
+
+
+    ?>
+    
+     
+
+  </body>
+</html>
