b04332c32198cfda44fa9fde8ec449cdce8169ca
Sivaldo Vieira <vieiralmeida.com@hotmail.com>
2023-12-07 10:33:58 -0300

    modifiquei pagina Array_pesquisa.php e criei pagina false_null_empty.php

--- Array_pesquisa.php
+++ Array_pesquisa.php
@@ -10,22 +10,48 @@
 <body>
   <h3>Funções</h3>
   <?php
-     //in_array()
-     //array_search()
+     //in_array() -> retorna true ou false para a existencia do que sta sendo pesqisado
+     //array_search() -> retorna o indice do valor pesquisado
 
-     $lista_frutas = ["banana","Maçã","Morango","Uva"];
+     $lista_frutas = ["Banana","Maçã","Morango","Uva"];
 
+    /*
 
-     echo"<pre>";
+     echo"<pre>"; #faz formatação do array
      print_r($lista_frutas);
      echo"<pre>";
 
-     echo in_array('Maçã', $lista_frutas);
+ 
+     //variavel existe recebe valor da pesquisa, se item existe ou não no array
+     //$existe = in_array('Pessego', $lista_frutas); #pesquisar item
 
-     
-      
+    //echo array_search("Banana", $lista_frutas);#exibe o indice de Banana
+    //echo array_search("Maçã", $lista_frutas);#exibe o indice de Maçã
+    $existe = array_search("Abacaxi", $lista_frutas);#exibe o indice de Uva
 
 
+    
+     if($existe != null) {
+      echo 'Sim, o valor pesquisado existe no array';
+     }else{
+      echo 'Não , o valor pesquisado não existe no array';
+
+     }
+
+     */
+
+
+     $lista_coisas = [
+      'frutas' => $lista_frutas,
+      'pessoas'=> ['Joao', 'Maria']
+     ];
+    
+
+      echo"<pre>"; #faz formatação do array
+      print_r($lista_coisas);
+      echo"<pre>";
+
+      echo in_array('Joao', $lista_coisas['pessoas']);
   ?>
 
 
---
+++ false_null_empty.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Curso PHP</title>
+</head>
+
+<body>
+  <h3>Funções</h3>
+  <?php
+     //false (true/false) tipo variavel boolean
+     //null e empty - valores especiais
+
+     $funcionario1 = null;
+     $funcionario2 = '';
+     $funcionario3 = false;
+
+     //valores null
+     if(is_null($funcionario1)) {
+       echo 'sim , a variavel é nulll'; 
+      } else {
+        echo 'Não , a variavel não é null';
+      }
+
+      echo'<br>';
+
+      if(is_null($funcionario2)) {
+        echo 'sim , a variavel é nulll'; 
+       } else {
+         echo 'Não , a variavel não é null';
+       }
+
+       echo'<br>';
+
+       if(is_null($funcionario3)) {
+        echo 'sim , a variavel é nulll'; 
+       } else {
+         echo 'Não , a variavel não é null';
+       }
+
+       echo'<hr>';
+      //valores vazios?
+       if(empty($funcionario1)) {
+        echo 'sim , a variavel esta vazia'; 
+       } else {
+         echo 'Não , a variavel não esta vazia';
+       }
+ 
+       echo'<br>';
+ 
+       if(empty($funcionario2)) {
+         echo 'sim , a variavel esta vazia'; 
+        } else {
+          echo 'Não , a variavel não esta vazia';
+        }
+
+        echo'<br>';
+
+        if(empty($funcionario3)) {
+          echo 'sim , a variavel esta vazia'; 
+         } else {
+           echo 'Não , a variavel não esta vazia';
+         }
+ 
+  ?>
+
+
+
+</body>
+
+</html>
