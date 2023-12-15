0ffe717b39c8b8471a779b9f9aec8ef8d1811ad4
Sivaldo Vieira <vieiralmeida.com@hotmail.com>
2023-12-15 19:04:59 -0300

    mudanças feitas na pagina index.php e valida_login.php

--- app_help_desk/valida_login.php
+++ app_help_desk/valida_login.php
@@ -1,3 +1,18 @@
 <?php 
-  echo"Oi estamos aqui!";
+/*
+  print_r($_GET);
+
+  echo"<br>";
+
+  echo $_GET["email"];
+  echo "<br>";
+  echo $_GET["senha"];
+  */
+
+  print_r($_POST);
+  echo"<br>";
+
+  echo $_POST["email"];
+  echo "<br>";
+  echo $_POST["senha"];
 ?>
