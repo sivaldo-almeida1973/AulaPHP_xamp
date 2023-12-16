7b6de01addfae9ee2bae7edcde7a9bbaec838a5e
Sivaldo Vieira <vieiralmeida.com@hotmail.com>
2023-12-16 13:23:14 -0300

    criado pagina registra_chamodo.php

---
+++ app_help_desk/registra_chamado.php
@@ -0,0 +1,31 @@
+<?php 
+
+   echo "<pre>";
+   print_r($_POST);
+   echo "</pre>";
+
+   //trabalhando na montagem do texto
+   $titulo = str_replace("#","-", $_POST["titulo"]);
+   $categoria = str_replace("#","-", $_POST["categoria"]);
+   $descricao = str_replace("#","-", $_POST["descricao"]);
+
+   //abrir um arquivo
+   $arquivo = fopen("arquivo.txt","a");
+
+
+
+   //implode('#', $_POST);
+   //definir o que vai ser escrito no arquivo
+   //  PHP_EOL , faz pular linhas no arquivo a cada chamado
+   $texto = $titulo .'#' . $categoria . '#' . $descricao . PHP_EOL;
+
+    //escrevendo em um arquivo
+   fwrite($arquivo, $texto);
+
+   fclose($arquivo);//fechar arquivo
+   //echo $texto;
+
+   header('Location: abrir_chamado.php');
+
+?>
+
