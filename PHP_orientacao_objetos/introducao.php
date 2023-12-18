8ab1c337934db03bcb2cb1747490ba49fd5b8625
Sivaldo Vieira <vieiralmeida.com@hotmail.com>
2023-12-18 11:50:30 -0300

    criado pasta PHP_orientacao_objetos/introducao.php

---
+++ PHP_orientacao_objetos/introducao.php
@@ -0,0 +1,19 @@
+<?php 
+   //introducao a Orientação a Objetos
+
+   //Paradigmas de Programação(padrao,forma de fazer)
+
+   //=======Procedural/Estruturado/Imperativo================
+
+   #Funcional-----------
+   #Lógico--------------
+
+
+   //============Orientado a Objetos=========================
+
+   #Concorrente--------
+   #Reativo------------
+
+
+
+?>
