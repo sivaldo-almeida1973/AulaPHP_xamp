d1a3eb0faeed9c76bea909318ce66feadd3903ff
Sivaldo Vieira <vieiralmeida.com@hotmail.com>
2023-12-18 12:41:51 -0300

    criada pagina PHP_orientacao_objetos/pilar_da_abstracao.php

---
+++ PHP_orientacao_objetos/pilar_da_abstracao.php
@@ -0,0 +1,16 @@
+<?php 
+  //=============Pilar da Abstração==================
+
+  # entidade ->      Produto,    ideia,        funcionario
+
+  # identidade -> new Porduto(), New ideia(), new Funcionario()
+  
+  # caracteristicas -> categoria , tiutlo, descricao, valor, tipo, descrição, nome, complexidade,telefone, numFilhos
+
+  # acoes -> exibirValores, exibirResumoProduto, alterarValorProduto, modificarTipoIdeia, resumirCadFunc, modificarNumFilhos
+
+
+
+
+
+?>
