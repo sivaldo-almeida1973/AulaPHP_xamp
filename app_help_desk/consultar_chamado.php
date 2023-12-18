e0da4f65f21eb72bd6ec531d0acbf2ab8e6b8a84
Sivaldo Vieira <vieiralmeida.com@hotmail.com>
2023-12-18 09:40:43 -0300

    mudanças feitas

--- app_help_desk/consultar_chamado.php
+++ app_help_desk/consultar_chamado.php
@@ -2,24 +2,44 @@
 
 <?php 
 
-   
-
    //declarar um array para guardar os chamados
     $chamados = array();
 
+    echo $_SESSION['perfil_id'];
+
    //abrir arquivo.txt
-    $arquivo = fopen("arquivo.txt","r");
+   // $arquivo = fopen("arquivo.txt","r");
+   $arquivo = fopen('arquivo.txt', 'r');
    
     //precorrer o arquivo.txt enquanto houver registros(linhas) a recuprerar
 
-    while (!feof($arquivo)) { //testa pelo fim de um arquivo
-      //linhas
-      $registro = fgets($arquivo);
-      $chamados[] = $registro ;  //inserir os registros recup do arquivo, dentro do array chamados
+    //enquanto houver registros (linhas) a serem recuperados
+  while(!feof($arquivo)) { //testa pelo fim de um arquivo
+    //linhas  
+    $registro = fgets($arquivo);
+
+    //explode dos detalhes do registro para verificar o id do usuário responsável pelo cadastro
+    $registro_detalhes = explode('#', $registro);
+
+    //(perfil id = 2) só vamos exibir o chamado, se ele foi criado pelo usuário
+    if($_SESSION['perfil_id'] == 2) {
+
+      //se usuário autenticado não for o usuário de abertura do chamado então não faz nada
+      if($_SESSION['id'] != $registro_detalhes[0]) {
+        continue; //não faz nada
+
+      } else {
+        $chamados[] = $registro; //adiciona o registro do arquivo ao array $chamados
+      }
+
+    } else {
+      $chamados[] = $registro; //adiciona o registro do arquivo ao array $chamados
     }
 
+  }
 
-    fclose($arquivo);//fechar arquivo
+  //fechar o arquivo aberto
+  fclose($arquivo);
 
  
 ?>
@@ -55,8 +75,7 @@
           <a class="nav-link" href="logoff.php">SAIR</a>
 
         </li>
-      </ul>
-      
+      </ul>      
     </nav>
 
     <div class="container">    
@@ -69,38 +88,27 @@
             </div>
             
             <div class="card-body">
+                <?php foreach($chamados as $chamado) { ?>
 
-            <?php foreach($chamados as $chamado) { ?>
+                  <?php 
 
-              <?php 
-
-                $chamado_dados = explode("#", $chamado);
-                
-                //
-                if($_SESSION['perfil_id'] == 2) {
-                   //só vamos exibir o chamado, se ele foi criado pelo usuario
-                    if($_SESSION['id'] != $chamado_dados[0]) {
+                    $chamado_dados = explode("#", $chamado);               
+              
+                    if (count($chamado_dados) < 3) {//indice vazio não sera listado
                       continue;
                     }
-                }
+                                  
+                  ?>             
+                  <div class="card mb-3 bg-light">
+                    <div class="card-body">
+                      <h5 class="card-title"><?=$chamado_dados[1]?></h5>
+                      <h6 class="card-subtitle mb-2 text-muted"><?=$chamado_dados[2]?></h6>
+                      <p class="card-text"><?=$chamado_dados[3]?></p>
 
-                if (count($chamado_dados) < 3) {//indice vazio não sera listado
-                  continue;
-                }
-                              
-              ?>             
-              <div class="card mb-3 bg-light">
-                <div class="card-body">
-                  <h5 class="card-title"><?=$chamado_dados[1]?></h5>
-                  <h6 class="card-subtitle mb-2 text-muted"><?=$chamado_dados[2]?></h6>
-                  <p class="card-text"><?=$chamado_dados[3]?></p>
+                    </div>
+                  </div>
 
-                </div>
-              </div>
-
-            <?php  } ?>
-
-              
+                <?php  } ?>
 
               <div class="row mt-5">
                 <div class="col-6">
