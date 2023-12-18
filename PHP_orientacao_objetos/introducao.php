3f6eda318ab642d6b7ee80884a603109717ec54f
Sivaldo Vieira <vieiralmeida.com@hotmail.com>
2023-12-18 12:29:58 -0300

    continuacao PHP_orientacao_objetos/introducao.php

--- PHP_orientacao_objetos/introducao.php
+++ PHP_orientacao_objetos/introducao.php
@@ -4,12 +4,48 @@
    //Paradigmas de Programação(padrao,forma de fazer)
 
    //=======Procedural/Estruturado/Imperativo================
+  
+   #chamada de procedimentos (procedures) para manipulacao de dados
+
+   $a = 10;
+   $b = 7;
+   $operador = 'soma';
+
+   function calcular($a, $b, $operador) {
+      if($operador == 'soma' ) {
+         return $a + $b;
+      }
+      return false;
+   }
+
+   echo calcular($a, $b , $operador);
 
    #Funcional-----------
    #Lógico--------------
 
-
+    echo "<hr>";
    //============Orientado a Objetos=========================
+
+   #paradigma de Orientação a Objetos
+
+
+   class Calculadora {
+      public $a = 10;
+      public $b = 8;
+      public $operador = 'soma';
+
+      public function calcular() {
+         if($this->operador == 'soma' ) {
+             return $this-> a + $this->b;
+         }
+         return false;
+      }
+   }
+   $calcular = new Calculadora();
+   echo $calcular-> calcular();
+
+
+
 
    #Concorrente--------
    #Reativo------------
