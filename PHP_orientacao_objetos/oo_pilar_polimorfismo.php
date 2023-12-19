8a6b71c4625c31d920e5a740c7d8dfd9a5eeb17d
Sivaldo Vieira <vieiralmeida.com@hotmail.com>
2023-12-19 10:33:43 -0300

    update na pagina oo_pilar_polimorfismo.php

---
+++ PHP_orientacao_objetos/oo_pilar_polimorfismo.php
@@ -0,0 +1,86 @@
+<?php 
+
+   class Carro extends Veiculo{
+      
+       public $teto_solar = true;
+
+       function __construct($placa, $cor) {
+           $this->placa = $placa;
+           $this->cor = $cor;
+
+       }
+        
+       function abrirTetoSolar() {
+        echo 'Abrindo teto solar';
+       }
+
+       function alterarPosicaoVolante() {
+           echo 'mudando posicao volante';
+       }     
+   }     //fim class carro-------------
+
+
+   class Moto extends Veiculo{  //inicio class moto-------------
+      
+       public $contra_peso_guidao = true;
+
+       function __construct($placa, $cor) {
+        $this->placa = $placa;
+        $this->cor = $cor;
+       }
+
+       function empinar() {
+        echo 'empinar';
+       }
+
+       function trocarMarcha() {
+        echo 'Moto:Desengatar embreagem com a mão e engatar marcha com o pé';
+  
+      }
+   }     //----------fim class moto-------------
+
+
+   class Veiculo {  //-inicio class veiculo-------------
+    public $placa = null;
+    public $cor = null;
+    
+    
+    function acelerar() {
+      echo 'acelerar ';
+    }
+
+    function freiar() {
+      echo 'freiando';
+    }
+
+    function trocarMarcha() {
+      echo 'Carro:Desengatar embreagem com o pé e engatar marcha com a mão';
+
+    }
+
+   }     //fim class veiculo-------------
+
+    
+   class Caminhao extends Veiculo {
+    
+   }
+
+   
+
+   $carro = new Carro('abc1234','branco');
+   $moto = new Moto('xyz4567','vermelha');
+   $caminhao = new Caminhao('xtz3333','Azul');
+
+
+  $carro->trocarMarcha();
+  echo'<br>';
+  $moto->trocarMarcha();
+  echo '<br>';
+  $caminhao->trocarMarcha();
+  
+
+   
+
+
+
+?>
