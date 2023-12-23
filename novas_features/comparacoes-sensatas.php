303fc3e8329f06be7079cddd9c87ea844b507866
Sivaldo Vieira <vieiralmeida.com@hotmail.com>
2023-12-23 13:08:00 -0300

    criada a pagina comparacoes-sensatas.php

---
+++ novas_features/comparacoes-sensatas.php
@@ -0,0 +1,15 @@
+<?php 
+    
+
+    
+    echo "<br>"; echo '0 == "0":' . (0 == "0");
+    echo "<br>"; echo '0 == "0.0":' . (0 == "0.0");
+    echo "<br>"; echo '0 == "teste":' . (0 == "teste");
+    echo "<br>"; echo '0 == "":' . (0 == "");
+    echo "<br>"; echo '50 == "  50":' . (50 == "  50");
+    echo "<br>"; echo '50 == "50teste":' . (0 == "50tetse");
+    echo "<br>"; echo '0 == "0":' . (0 == "0");
+   
+
+
+?>
